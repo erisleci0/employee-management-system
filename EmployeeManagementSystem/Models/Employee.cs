3385192a94d92b81837e765e6bca71da43c72db6
Eris Leci <lecieris@hotmail.com>
2022-11-01 16:43:51 +0100

    First version of my project

---
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class Employee
+    {
+        public virtual int EmployeeId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual int PhoneNumber { get; set; }
+    }
+}
